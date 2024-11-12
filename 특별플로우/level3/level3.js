af2fbc9ad50a1d86f5c9f0f352e5c9e94f9bc151
Jungyu Park <parkjungyu09@gmail.com>
2024-11-12 18:39:48 +0900

    Recipe add quiz level3

--- 특별플로우/level3/level3.js
+++ 특별플로우/level3/level3.js
@@ -1,3 +1,46 @@
-/* 
-레시피 재료 추가하기
-*/
+const Table = document.querySelector("table");
+const ingridientForm = document.querySelector("#ingredient-form");
+
+ingridientForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const ingredient = event.currentTarget.querySelector(
+    "input[name='ingredient']"
+  ).value;
+  const weight = event.currentTarget.querySelector(
+    "input[name='weight']"
+  ).value;
+
+  const existingIngredient = Array.from(
+    Table.querySelectorAll("tr td:first-child")
+  ).map((td) => td.textContent);
+
+  if (existingIngredient.includes(ingredient)) return alert("이미 있는 재료임");
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "삭제";
+
+  deleteBtn.addEventListener("click", (event) => {
+    TableBody.remove(event.target.closest("tr"));
+  });
+
+  const TableBody = document.createElement("tr");
+  TableBody.innerHTML = `<td>${ingredient}</td>
+    <td>${weight}</td>
+    `;
+
+  const deleteCell = document.createElement("td");
+  deleteCell.appendChild(deleteBtn);
+  TableBody.appendChild(deleteCell);
+
+  Table.appendChild(TableBody);
+  const ingredientList = document.querySelector("#ingredient-list");
+  const submitBtn = document.querySelector("#submit_button");
+
+  submitBtn.addEventListener("click", () => {
+    const li = document.createElement("li");
+    li.innerText = `${ingredient}: ${weight}`;
+    ingredientList.appendChild(li);
+  });
+
+});
