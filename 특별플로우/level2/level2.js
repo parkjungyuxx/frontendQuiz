bc8f86a647000daadd017e6aaa88d5ac75792ae8
Jungyu Park <parkjungyu09@gmail.com>
2024-11-12 17:47:21 +0900

    reservation level2

--- 특별플로우/level2/level2.js
+++ 특별플로우/level2/level2.js
@@ -1,8 +1,26 @@
-import { RESERVATION_LIST } from './reservation .js';
+import { RESERVATION_LIST } from "./reservation .js";
 console.log(RESERVATION_LIST);
 
-/* 
-예약 고객확인하기
+const reservationNumberList = document.querySelector("#reservation-number");
 
+const Form = document.querySelector("form");
 
-*/
+Form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  reservationNumberList.innerHTML = "";
+  const UserName = event.target["user-name"].value;
+  const UserPhone = event.target["user-phone"].value;
+
+  const checkReservationNumber = document.createElement("li");
+  checkReservationNumber.textContent = "";
+
+  const UserObj = RESERVATION_LIST.find((obj) => obj.name === UserName);
+  if (UserObj.phone === UserPhone) {
+    checkReservationNumber.textContent = UserObj.number;
+  } else {
+    checkReservationNumber.textContent = "일치하는 항목이 없습니다";
+    alert("일치하는 항목이 없습니다");
+  }
+
+  reservationNumberList.appendChild(checkReservationNumber);
+});
